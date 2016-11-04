9f754144be1c50e72bb1e74fee2ccd0dd7266568
veenaw <vwagle2@gmail.com>
2016-11-04 15:36:19 +0530

    Auto intent

--- app/src/main/java/com/example/android/datamanagementpreffragment/ViewAndUpdatePreferencesActivity.java
+++ app/src/main/java/com/example/android/datamanagementpreffragment/ViewAndUpdatePreferencesActivity.java
@@ -2,12 +2,13 @@ package com.example.android.datamanagementpreffragment;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.os.Bundle;
 
 public class ViewAndUpdatePreferencesActivity extends Activity {
     private static final String USERNAME = "uname";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,46 +16,46 @@ public class ViewAndUpdatePreferencesActivity extends Activity {
     }
 
 
-// Fragment that displays the username preference
-public static class UserPreferenceFragment extends PreferenceFragment {
+    // Fragment that displays the username preference
+    public static class UserPreferenceFragment extends PreferenceFragment {
 
-    protected static final String TAG = "UserPrefsFragment";
-    private SharedPreferences.OnSharedPreferenceChangeListener mListener;
-    private Preference mUserNamePreference;
+        protected static final String TAG = "UserPrefsFragment";
+        private SharedPreferences.OnSharedPreferenceChangeListener mListener;
+        private Preference mUserNamePreference;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
 
-        // Load the preferences from an XML resource
-        addPreferencesFromResource(R.xml.user_prefs);
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.user_prefs);
 
-        // Get the username Preference
-        mUserNamePreference = (Preference) getPreferenceManager()
-                .findPreference(USERNAME);
+            // Get the username Preference
+            mUserNamePreference = getPreferenceManager()
+                    .findPreference(USERNAME);
 
-        // Attach a listener to update summary when username changes
-        mListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
-            @Override
-            public void onSharedPreferenceChanged(
-                    SharedPreferences sharedPreferences, String key) {
-                mUserNamePreference.setSummary(sharedPreferences.getString(
-                        USERNAME, "None Set"));
-            }
-        };
+            // Attach a listener to update summary when username changes
+            mListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+                @Override
+                public void onSharedPreferenceChanged(
+                        SharedPreferences sharedPreferences, String key) {
+                    mUserNamePreference.setSummary(sharedPreferences.getString(
+                            USERNAME, "None Set"));
+                }
+            };
 
-        // Get SharedPreferences object managed by the PreferenceManager for
-        // this Fragment
-        SharedPreferences prefs = getPreferenceManager()
-                .getSharedPreferences();
+            // Get SharedPreferences object managed by the PreferenceManager for
+            // this Fragment
+            SharedPreferences prefs = getPreferenceManager()
+                    .getSharedPreferences();
 
-        // Register a listener on the SharedPreferences object
-        prefs.registerOnSharedPreferenceChangeListener(mListener);
+            // Register a listener on the SharedPreferences object
+            prefs.registerOnSharedPreferenceChangeListener(mListener);
 
-        // Invoke callback manually to display the current username
-        mListener.onSharedPreferenceChanged(prefs, USERNAME);
+            // Invoke callback manually to display the current username
+            mListener.onSharedPreferenceChanged(prefs, USERNAME);
+
+        }
 
     }
-
-}
 }
